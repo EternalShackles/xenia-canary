4d6c2e033c6a5df247a6d175459f0ec2b6abe892
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-06 16:16:19 +0000

    [XLiveAPI/APP] Improved XLiveAPI initialisation & Netplay toolbar tab
    Improved XLiveAPI initialisation
    Added Netplay toolbar tab

--- src/xenia/app/emulator_window.h
+++ src/xenia/app/emulator_window.h
@@ -231,6 +231,7 @@ class EmulatorWindow {
                          bool vibrate = true);
   void GamepadHotKeys();
   void ToggleGPUSetting(gpu::GPUSetting setting);
+  void NetplayStatus();
   void DisplayHotKeysConfig();
 
   static std::string CanonicalizeFileExtension(
