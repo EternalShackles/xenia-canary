0d6b7443d516685e48a771a1364d47b80721fc77
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-12-14 16:33:35 +0000

    [XAM] Support online XUIDs

--- src/xenia/kernel/xboxkrnl/xboxkrnl_xconfig.cc
+++ src/xenia/kernel/xboxkrnl/xboxkrnl_xconfig.cc
@@ -139,6 +139,10 @@ X_STATUS xeExGetXConfigSetting(uint16_t category, uint16_t setting,
           setting_size = 1;
           value[0] = static_cast<uint8_t>(cvars::user_country);
           break;
+        case 0x000F:  // XCONFIG_USER_PC_FLAGS (Parental Control Flags)
+          setting_size = 1;
+          xe::store_and_swap<uint8_t>(value, 0);
+          break;
         default:
           XELOGW("An unimplemented setting 0x{:04X} in XCONFIG USER CATEGORY",
                  static_cast<uint16_t>(setting));
